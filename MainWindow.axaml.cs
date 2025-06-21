515e764e0ffdbb66a665698588737edeab949b76
Leandro G. da Cruz <LeandroCruz717@outlook.com>
2025-06-20 21:09:45 -0400

    feat: implement pause and resume

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -21,15 +21,35 @@ public partial class MainWindow : Window
         {
             PauseTimerButton.IsVisible = false;
             StartTimerButton.Content = "Start Timer";
+            PauseTimerButton.Content = "Pause Timer";
+        };
+        _timerManager.OnTimerPause += () =>
+        {
+            PauseTimerButton.Content = "Resume Timer";
+        };
+        _timerManager.OnTimerResume += () =>
+        {
+            PauseTimerButton.Content = "Pause Timer";
         };
     }
 
     public void StartTimer(object sender, RoutedEventArgs e)
     {
         if (_timerManager.IsActive)
-            _timerManager.Stop();
+            _timerManager.Reset();
         else
             _timerManager.Start();
+    }
+
+    public void PauseTimer(object sender, RoutedEventArgs e)
+    {
+        if (!_timerManager.IsActive)
+            return;
+
+        if (_timerManager.IsPaused)
+            _timerManager.Resume();
+        else
+            _timerManager.Pause();
     }
 
     public void UpdateTimer(TimerTime time)
--- Timer.cs
+++ Timer.cs
@@ -5,8 +5,7 @@ namespace widgets;
 
 public record TimerData(
     DispatcherTimer Dispatcher,
-    DateTime TimerStart,
-    TimeSpan TimePaused
+    DateTime TimerStart
 );
 
 public record TimerTime(TimeSpan Time)
@@ -19,8 +18,37 @@ public record TimerTime(TimeSpan Time)
 
 public class TimerPause
 {
-    public DateTime PausedAt { get; } = DateTime.UtcNow;
-    public TimeSpan TimePaused => DateTime.UtcNow - PausedAt;
+    private DateTime? _pausedAt;
+    public TimeSpan TimePaused { get; private set; }
+    public bool IsPaused => _pausedAt is not null;
+
+    public void Start()
+    {
+        _pausedAt ??= DateTime.UtcNow;
+    }
+
+    public void Reset()
+    {
+        _pausedAt = new DateTime();
+        TimePaused = TimeSpan.Zero;
+    }
+
+    public void Finish()
+    {
+        TimePaused = GetTimePaused();
+        _pausedAt = null;
+    }
+
+    private TimeSpan GetTimePaused()
+    {
+        if (_pausedAt is null)
+            return TimeSpan.Zero;
+
+        var currentTimePaused = DateTime.UtcNow - _pausedAt;
+        var totalTimePaused = TimePaused + currentTimePaused;
+
+        return (TimeSpan)totalTimePaused;
+    }
 }
 
 public class TimerManager
@@ -28,24 +56,29 @@ public class TimerManager
     public delegate void OnTimerUpdate(TimerTime Time);
     public delegate void OnTimerStart();
     public delegate void OnTimerStop();
+    public delegate void OnPause();
+    public delegate void OnResume();
 
     public event OnTimerUpdate? OnTimerUpdateEvent;
     public event OnTimerStart? OnTimerStartEvent;
     public event OnTimerStop? OnTimerStopEvent;
+    public event OnPause? OnTimerPause;
+    public event OnResume? OnTimerResume;
 
     private TimerData? _data;
+    private readonly TimerPause _pause = new();
 
     public bool IsActive => _data is not null;
+    public bool IsPaused => _pause.IsPaused;
 
     public void Start()
     {
-        if (_data is not null)
-            Stop();
+        if (_data is not null || _pause.IsPaused)
+            return;
 
         _data = new TimerData(
             new DispatcherTimer(TimeSpan.FromMilliseconds(500), DispatcherPriority.Background, (s, e) => HandleTimerUpdate()),
-            DateTime.UtcNow.AddSeconds(-1),
-            TimeSpan.Zero
+            DateTime.UtcNow.AddSeconds(-1)
         );
 
         _data.Dispatcher.Start();
@@ -54,13 +87,37 @@ public class TimerManager
         HandleTimerUpdate();
     }
 
-    public void Stop()
+    public void Resume()
+    {
+        if (_data is null)
+            return;
+
+        _data.Dispatcher.Start();
+        _pause.Finish();
+        OnTimerResume?.Invoke();
+        HandleTimerUpdate();
+    }
+
+    public void Pause()
+    {
+        if (_data is null)
+            return;
+            
+        _pause.Start();
+        _data.Dispatcher.Stop();
+        OnTimerPause?.Invoke();
+    }
+
+    public void Reset()
     {
         if (_data is null)
             return;
 
         _data.Dispatcher.Stop();
         _data = null;
+
+        _pause.Reset();
+
         OnTimerStopEvent?.Invoke();
 
         HandleTimerUpdate();
@@ -76,7 +133,7 @@ public class TimerManager
         if (_data is not null)
         {
             var now = DateTime.UtcNow;
-            timerTime = new TimerTime(now - _data.TimerStart);
+            timerTime = new TimerTime(now - _data.TimerStart - _pause.TimePaused);
         }
 
         OnTimerUpdateEvent.Invoke(timerTime);

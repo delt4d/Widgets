85efe465f08dff357e82270e65ff9d10a9fbc09d
Leandro G. da Cruz <LeandroCruz717@outlook.com>
2025-06-24 21:08:03 -0400

    style: move project to Widgets folder

---
+++ Widgets/Features/Timer/TimerWindow.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace widgets;
+
+public partial class TimerWindow : Window
+{
+    public TimerWindow()
+    {
+        InitializeComponent();
+    }
+}

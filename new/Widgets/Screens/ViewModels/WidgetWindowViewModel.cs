30d2f2cc3084bec4b347b4f1036b8a7abc5691f9
Leandro G. da Cruz <LeandroCruz717@outlook.com>
2025-06-30 18:24:45 -0400

    chore: remove address url observable property

--- new/Widgets/Screens/ViewModels/WidgetWindowViewModel.cs
+++ new/Widgets/Screens/ViewModels/WidgetWindowViewModel.cs
@@ -1,10 +1,7 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
-using Widgets.ViewModels;
+﻿using Widgets.ViewModels;
 
 namespace Widgets.Screens.ViewModels;
 
 public partial class WidgetWindowViewModel : ViewModelBase
 {
-    [ObservableProperty]
-    private string addressUrl = "https://google.com/";
 }
